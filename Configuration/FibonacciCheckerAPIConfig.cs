9c0d6cb6cd92218ca4a15354e1a49c706a2a8548
Toni Kuokkanen <tonikuokkanen@hotmail.com>
2020-11-11 13:31:24 +0200

    Add project files.

---
+++ Configuration/FibonacciCheckerAPIConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FibonacciCheckerAPI.Configuration
+{
+    public class FibonacciCheckerAPIConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.EnableCors();
+
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{number}",
+                defaults: new { number = RouteParameter.Optional }
+                );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+        }
+    }
+}
---
+++ Controllers/FibonacciController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+using System.Web.Http.Cors;
+
+namespace FibonacciCheckerAPI.Controllers
+{   
+    [EnableCors(origins: "http://localhost:44320", headers: "*", methods: "get")]
+    public class FibonacciController : ApiController
+    {
+      
+      //JSON-response
+      public JObject Get(int number)
+        {
+           String str = "";
+
+            if (isFibonacci(number) == true)
+            {
+                str = "{ \"answer\":\"Number belongs to the Fibonacci sequence\"}"; 
+            } else
+            {
+                str = "{ \"answer\":\"Number does NOT belong to the Fibonacci sequence\"}";
+            }
+            JObject response = JObject.Parse(str);
+            return response;
+        }
+
+        //Checks if given number belongs to Fibonacci sequence
+        static Boolean isFibonacci(int numberToCheck)
+        {
+            int a = 0;
+            int b = 1;
+            if (numberToCheck == a || numberToCheck == b) return true;
+            int c = a + b;
+            while (c <= numberToCheck)
+            {
+                if (c == numberToCheck) return true;
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            return false;
+        }
+        
+    }
+}
